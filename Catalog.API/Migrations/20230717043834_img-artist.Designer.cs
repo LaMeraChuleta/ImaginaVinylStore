82d0cb6808c5d216493338ccc0ffcdab7cfdc8e5
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-08-03 23:50:23 -0600

    Migrations not Zero

--- Catalog.API/Migrations/20230717043834_img-artist.Designer.cs
+++ Catalog.API/Migrations/20230717043834_img-artist.Designer.cs
@@ -13,7 +13,9 @@ namespace Catalog.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20230717043834_img-artist")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class imgartist
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
