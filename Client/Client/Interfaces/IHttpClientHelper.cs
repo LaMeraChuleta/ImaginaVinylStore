8b39aed539dbd2856df29f511edc486f28751c85
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-08-07 20:22:02 -0600

    Exten HttpClient for generic request

---
+++ Client/Client/Interfaces/IHttpClientHelper.cs
@@ -0,0 +1,12 @@
+﻿using SharedApp.Models;
+
+namespace Client.App.Interfaces;
+
+public interface IHttpClientHelper
+{
+    void ConfigureAuthorizationHeaderAsync();
+    Task<List<T>> Get<T>(string pathEndPoint);
+    Task<T> Post<T>(string pathEndPoint, T data);
+    Task<ImageArtist> PostImageArtist(string pathEndPoint, MultipartFormDataContent data);
+    Task<ImageCatalog> PostImageCatalog(string pathEndPoint, MultipartFormDataContent data);
+}
