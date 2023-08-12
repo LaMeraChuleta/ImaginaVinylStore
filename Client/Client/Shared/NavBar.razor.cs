548b5805c427ff2fd6449f75e958d89c9960e657
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-08-12 11:13:34 -0600

    fet(web): add partial view navbar event for search

---
+++ Client/Client/Shared/NavBar.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.App.Shared;
+
+public partial class NavBar : ComponentBase
+{
+    public delegate void SearchCatalogHandler(string query);
+
+    private bool ShowLeftBar { get; set; }
+    private bool ShowSearchInput { get; set; }
+    private string QuerySearch { get; set; } = string.Empty;
+
+    [Parameter] public EventCallback SendOpenLeftBar { get; set; }
+    [Parameter] public EventCallback<string> SendRedirectPage { get; set; }
+    [Parameter] public EventCallback<bool> SendOpenSearchList { get; set; }
+
+    public static event SearchCatalogHandler OnSearchCatalog;
+
+    private void ChangeShowSearch()
+    {
+        ShowSearchInput = !ShowSearchInput;
+        SendOpenSearchList.InvokeAsync(ShowSearchInput);
+    }
+    private void SearchCatalog(ChangeEventArgs e)
+    {
+        if(string.IsNullOrEmpty(e.Value!.ToString()))
+            OnSearchCatalog?.Invoke(e.Value.ToString()!);
+    }
+}
