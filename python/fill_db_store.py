667609f28b6702b190e77725135f1058181a0293
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-08-24 23:24:03 -0600

    feat:(db) nueva version del scrip para llenar el catalogoMusic

--- python/fill_db_store.py
+++ python/fill_db_store.py
@@ -4,7 +4,7 @@ import datetime
 
 def get_id_artist(artist_df, artist_text):
     if artist_df['Name'].isin([artist_text]).any():
-        return int(artist_df.loc[artist_df['Name'] == artist_text, 'Id'].values[0])
+        return int(artist_df.loc[artist_df['Name'] == artist_text, 'Id'].values[0])    
     else:        
         return int(create_artist(artist_text))
     
@@ -16,11 +16,11 @@ def create_artist(artist_name):
     # Datos del nuevo artista
     new_artist_country = "Mexico"
     # Consulta para insertar el nuevo artista
-    insert_query = "INSERT INTO Artists (Name, Country) VALUES (?, ?)"
+    insert_query = "INSERT INTO Artist (Name, Country) VALUES (?, ?)"
     cursor.execute(insert_query, artist_name, new_artist_country)
     connSql.commit()
     # Obtener el último ID insertado
-    cursor.execute("SELECT SCOPE_IDENTITY() AS NewArtistID")
+    cursor.execute("SELECT @@IDENTITY  AS NewArtistID")
     new_artist_id = cursor.fetchone().NewArtistID
     connSql.close()
     return new_artist_id
@@ -29,7 +29,7 @@ def create_artist(artist_name):
 
 def get_id_genre(genre_df, genre_text):
     if genre_df['Name'].isin([genre_text]).any():
-        return int(genre_df.loc[genre_df['Name'] == genre_text, 'Id'].values[0])
+        return int(genre_df.loc[genre_df['Name'] == genre_text, 'Id'].values[0])    
     else:        
         return int(create_genre(genre_text))
     
@@ -39,11 +39,11 @@ def create_genre(genre_name):
     connSql = pyodbc.connect(connection_string)
     cursor = connSql.cursor()        
     # Consulta para insertar el nuevo artista
-    insert_query = "INSERT INTO Genres (Name) VALUES (?)"
+    insert_query = "INSERT INTO Genre (Name) VALUES (?)"
     cursor.execute(insert_query, genre_name)
     connSql.commit()
     # Obtener el último ID insertado
-    cursor.execute("SELECT SCOPE_IDENTITY() AS NewGenreID")
+    cursor.execute("SELECT @@IDENTITY  AS NewGenreID")
     new_genre_id = cursor.fetchone().NewGenreID
     connSql.close()
     return new_genre_id
@@ -61,11 +61,11 @@ def create_format(format_name):
     connSql = pyodbc.connect(connection_string)
     cursor = connSql.cursor()        
     # Consulta para insertar el nuevo artista
-    insert_query = "INSERT INTO Formats (Name) VALUES (?)"
+    insert_query = "INSERT INTO Format (Name) VALUES (?)"
     cursor.execute(insert_query, format_name)
     connSql.commit()
     # Obtener el último ID insertado
-    cursor.execute("SELECT SCOPE_IDENTITY() AS NewFormatID")
+    cursor.execute("SELECT @@IDENTITY  AS NewFormatID")
     new_format_id = cursor.fetchone().NewFormatID
     connSql.close()
     return new_format_id
@@ -88,7 +88,7 @@ def create_presentation(presentation_name):
     cursor.execute(insert_query, presentation_name, 1)
     connSql.commit()
     # Obtener el último ID insertado
-    cursor.execute("SELECT SCOPE_IDENTITY() AS NewFormatID")
+    cursor.execute("SELECT @@IDENTITY  AS NewFormatID")
     new_artist_id = cursor.fetchone().NewFormatID
     connSql.close()
     return new_artist_id
@@ -96,10 +96,10 @@ def create_presentation(presentation_name):
 
 connection_string = "DRIVER=SQL Server;SERVER=localhost;DATABASE=test;UID=sa;PWD=VacaLoca69"
 # Query a ejecutar
-query_artist = "SELECT * FROM Artists"
-query_genre = "SELECT * FROM Genres"
-query_format = "SELECT * FROM Formats"
-query_presentation = "SELECT * FROM Presentations"
+query_artist = "SELECT * FROM Artist"
+query_genre = "SELECT * FROM Genre"
+query_format = "SELECT * FROM Format"
+query_presentation = "SELECT * FROM Presentation"
 
 try:
     # Intentar establecer la conexión y ejecutar la consulta de los catalogos
@@ -130,9 +130,47 @@ try:
         Label = col["Label"]
         CreateAt = datetime.datetime.now()    
 
-        
+        query_insert_catalog_music = f'''
+        INSERT INTO MusicCatalog (
+            Title, 
+            ArtistId, 
+            GenreId, 
+            FormatId, 
+            PresentationId,
+            Country,
+            [Year],
+            StatusCover,
+            StatusGeneral,
+            Price,
+            Matrix,
+            Label,
+            CreateAt)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+        '''
+
+        connection_string = "DRIVER=SQL Server;SERVER=localhost;DATABASE=test;UID=sa;PWD=VacaLoca69"
+        connSql = pyodbc.connect(connection_string)
+        cursor = connSql.cursor()    
+
+        cursor.execute(query_insert_catalog_music, 
+            Title, 
+            ArtistName,
+            GenreId , 
+            FormatId,
+            PresentationId,
+            country,
+            Year_Db, 
+            StatusCover,
+            StatusGeneral,
+            Price,
+            Matrix,
+            Label,
+            CreateAt)
+        connSql.commit()
+
+        # Consulta para insertar el nuevo artista   
         test = f'''
-        INSERT INTO MusicCatalogs (
+        INSERT INTO MusicCatalog (
             Title, 
             ArtistId, 
             GenreId, 
@@ -160,52 +198,10 @@ try:
             '{Matrix}',
             '{Label}',
             '{CreateAt}')
-        '''
-
-        query_insert_catalog_music = f'''
-        INSERT INTO MusicCatalogs (
-            Title, 
-            ArtistId, 
-            GenreId, 
-            FormatId, 
-            PresentationId,
-            Country,
-            [Year],
-            StatusCover,
-            StatusGeneral,
-            Price,
-            Matrix,
-            Label,
-            CreateAt)
-        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
-        '''
-
-        connection_string = "DRIVER=SQL Server;SERVER=localhost;DATABASE=test;UID=sa;PWD=VacaLoca69"
-        connSql = pyodbc.connect(connection_string)
-        cursor = connSql.cursor()        
-        # Consulta para insertar el nuevo artista    
-        print(test)    
-
-        cursor.execute(query_insert_catalog_music, 
-            Title, 
-            ArtistName,
-            GenreId , 
-            FormatId,
-            PresentationId,
-            country,
-            Year_Db, 
-            StatusCover,
-            StatusGeneral,
-            Price,
-            Matrix,
-            Label,
-            CreateAt)
-        connSql.commit()
-
-        print(query_insert_catalog_music)
-    
+        ''' 
+        print(test)        
     
 
-except pyodbc.Error as e:
-    print(f"Ocurrió un error: {e}")
+except pyodbc.Error as e:    
+    print(f"Ocurrió un error: {e}")    
 
