1c0abd31acf7411413e67e5a948cf918d19788c8
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-09-07 00:21:34 -0600

    feat:(web) iterface notification ShoptCart

--- Client/Client/Interfaces/IShopCartNotificationService.cs
+++ Client/Client/Interfaces/IShopCartNotificationService.cs
@@ -2,8 +2,7 @@
 {
     public interface IShopCartNotificationService
     {
-        delegate void ShopCartCountUpdate(int shopCartCount);
-        static event ShopCartCountUpdate OnShopCartCountUpdate;
+        delegate void ShopCartCountUpdate(int shopCartCount);        
         void NotifitShopCartCountChanges(int shopCartCount);
     }
 }
