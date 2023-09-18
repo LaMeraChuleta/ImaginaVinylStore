3d63d93407c9ada90c6e068fd02f50798e6fdd80
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-09-17 21:45:26 -0600

    feat:(web) cambios en controlladores y controladores nuevos para pagos

---
+++ Catalog.API/Controllers/CheckoutController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+using System.Security.Claims;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CheckoutController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Post()
+        {
+
+            var id = _httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            var domain = "http://localhost:7285";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = "price_1NplPQF4FZD9gDsPuj6AdpwF",
+                    Quantity = 1
+                  },
+                     new SessionLineItemOptions
+                  {
+                    Price = "price_1NplPQF4FZD9gDsPuj6AdpwF",
+                    Quantity = 1
+                  }
+                },
+                ClientReferenceId = id,
+                //Metadata = new Dictionary<string, string>()
+                //{
+                //    { "idUser", id }
+                //},
+                Mode = "payment",
+                SuccessUrl = domain + "/success.html",
+                CancelUrl = domain + "/cancel.html",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+            Response.Headers.Add("Location", session.Url);
+            return Ok(session.Url);
+        }
+    }
+}
---
+++ Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedApp.Data;
+using SharedApp.Models;
+using Stripe;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IResult Post([FromBody] MusicCatalog value)
+        {
+            if (!ModelState.IsValid) return Results.BadRequest();
+
+            var images = _context.ImageCatalog
+                .Where(x => x.MusicCatalogId == value.Id)
+                .Select(x => x.Url)
+                .ToList();
+
+            var options = new ProductCreateOptions()
+            {
+                Name = $"{value.Title}-{value.Artist!.Name}",
+                Images = images,
+                DefaultPriceData = new ProductDefaultPriceDataOptions()
+                {
+                    UnitAmount = value.Price,
+                    Currency = "mxn"
+                }
+            };
+            var service = new ProductService();
+            service.Create(options);
+
+            _context.MusicCatalog.Add(value);
+            _context.SaveChanges();
+            return Results.Ok(value);
+        }
+    }
+}
