075c39276d410fc382915653f2274841061753c2
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-11-27 22:36:40 -0600

    feat:(api) controller checkout parameter

--- Client/Client/Pages/CartSummary.razor.cs
+++ Client/Client/Pages/CartSummary.razor.cs
@@ -25,15 +25,16 @@ namespace Client.App.Pages
             var shopCarts = await ShopCartService.GetShopCart();
 
             //await HttpClientHelperService.Post<string>("Checkout", data);
-            var data = shopCarts
-                .Select((value, index) => new
-                {
-                    Key = value.IsMusicCatalog ? $"music-{index}" : $"audio-{index}",
-                    Value = value.Id.ToString()
-                })
-                .ToDictionary(pair => pair.Key, pair => pair.Value);
+            //var data = shopCarts
+            //    .Select((value, index) => new
+            //    {
+            //        Key = value.IsMusicCatalog ? $"music-{index}" : $"audio-{index}",
+            //        Value = value.Id.ToString()
+            //    })
+            //    .ToDictionary(pair => pair.Key, pair => pair.Value);
 
-            var url = await HttpClientHelperService.Post("Checkout", data);
+            //var url = await HttpClientHelperService.Post("Checkout", data);
+            var url = await HttpClientHelperService.Post("Checkout", (object)shopCarts);
             NavigationManager.NavigateTo(url);
         }
     }
