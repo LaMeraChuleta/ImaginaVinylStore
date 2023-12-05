8e42ebfb23bbadddafead444e6277abf9b1dccbc
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-12-04 18:43:15 -0600

    feat:(format)

--- Client/Client/Interfaces/IHttpClientHelperService.cs
+++ Client/Client/Interfaces/IHttpClientHelperService.cs
@@ -2,8 +2,8 @@
 public interface IHttpClientHelperService
 {
     Task<T> Get<T>(string pathEndPoint);
-    Task<T> Get<T>(string pathEndPoint, Dictionary<string, string> parameters);    
-    Task<T> Post<T>(string pathEndPoint, T data);    
+    Task<T> Get<T>(string pathEndPoint, Dictionary<string, string> parameters);
+    Task<T> Post<T>(string pathEndPoint, T data);
     Task<string> Post(string pathEndPoint, object data);
     Task<T> Put<T, U>(string pathEndPoint, int id, U data);
     Task<T> Post<T>(string pathEndPoint, MultipartFormDataContent data);
