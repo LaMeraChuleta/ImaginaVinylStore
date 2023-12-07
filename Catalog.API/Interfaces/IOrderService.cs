4ffa1106ce446b78c9d224a54a08667d03542457
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-12-06 21:20:51 -0600

    feat:(api) order service create y get

---
+++ Catalog.API/Interfaces/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<bool> CreateAsync(Session session);
+        Task<IEnumerable<Order>> GetAsync(string idAspNetUser);
+    }
+}
