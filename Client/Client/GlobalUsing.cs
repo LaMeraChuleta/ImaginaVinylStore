a908a9614689de7c0aa59efef72e8f6c050e1a7b
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-12-28 19:30:46 -0600

    fix:(web) componete ref

--- Client/Client/GlobalUsing.cs
+++ Client/Client/GlobalUsing.cs
@@ -1,20 +1,15 @@
-﻿global using SharedApp.Validation;
-global using SharedApp.Models;
-global using SharedApp.Extension;
-
-global using Client.App;
-global using Client.App.Pages;
-global using Client.App.Services;
-global using Client.App.Interfaces;
-
-global using Blazored.LocalStorage;
+﻿global using Blazored.LocalStorage;
 global using Blazored.Toast;
 global using Blazored.Toast.Services;
-
-global using System.Net;
-global using System.Net.Http.Json;
-global using System.Net.Http.Headers;
-
+global using Client.App;
+global using Client.App.Interfaces;
+global using Client.App.Services;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+global using SharedApp.Extension;
+global using SharedApp.Models;
+global using SharedApp.Validation;
+global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Net.Http.Json;
