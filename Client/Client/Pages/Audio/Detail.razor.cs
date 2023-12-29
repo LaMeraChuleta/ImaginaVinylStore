bdcbc5ef5dde5f3b8b591af0e9ff2056bdedb4c9
LaMeraChuleta <geoffreyytorres25@gmail.com>
2023-12-28 21:36:19 -0600

    feat:(web) Primera version detalle Audio

---
+++ Client/Client/Pages/Audio/Detail.razor.cs
@@ -0,0 +1,45 @@
+﻿namespace Client.App.Pages.Audio
+{
+    public partial class Detail : ComponentBase
+    {
+        [Parameter] public int IdAudioCatalog { get; set; }
+        [Inject] public IAudioCatalogService CatalogAudioService { get; set; }
+        [Inject] public IToastService ToastService { get; set; }
+        [Inject] public IShopCartService ShopCartService { get; set; }
+
+        private AudioCatalog AudioCatalog { get; set; }        
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                AudioCatalog = await CatalogAudioService.GetByIdAsync(IdAudioCatalog);                
+            }
+            catch (Exception ex)
+            {
+                ToastService.ShowToast(ToastLevel.Error, ex.Message);
+            }
+
+            await base.OnInitializedAsync();
+        }
+
+        private async void AddItemShopCart()
+        {
+            try
+            {
+                var isItemCreate = await ShopCartService.SetShopCartItem(AudioCatalog);
+                if (isItemCreate)
+                {
+                    ToastService.ShowSuccess($"Se guardo este articulo en el carrito. {AudioCatalog.Name}-{AudioCatalog.Brand}");
+                }
+                else
+                {
+                    ToastService.ShowWarning($"Ya existe este articulo en el carrito. {AudioCatalog.Name}-{AudioCatalog.Brand}");
+                }
+            }
+            catch (Exception exception)
+            {
+                ToastService.ShowToast(ToastLevel.Error, exception.Message);
+            }
+        }
+    }
+}
