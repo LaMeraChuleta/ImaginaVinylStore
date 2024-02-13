796bd39bbb039ea6b258ebeb901943f074321f01
LaMeraChuleta <geoffreyytorres25@gmail.com>
2024-02-13 17:16:57 -0600

    feat:(web) reducioenda carga de catalogo

--- Catalog.API/Controllers/MusicCatalogController.cs
+++ Catalog.API/Controllers/MusicCatalogController.cs
@@ -22,7 +22,7 @@ public class MusicCatalogController : ControllerBase
             .Include(x => x.Genre)
             .Include(x => x.Presentation)
             .Include(x => x.Format)
-            .Include(x => x.Images)
+            .Include(x => x.Images)            
             .ToArray());
     }
 
