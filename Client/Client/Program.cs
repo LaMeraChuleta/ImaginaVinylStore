3c1b379c6d8daea020b88f81d75d0b1dbbcd1a8c
LaMeraChuleta <geoffreyytorres25@gmail.com>
2024-02-12 21:45:18 -0600

    feat:(conf) nuevas cadenas para publicacion azure

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -8,8 +8,19 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient("Client.ServerAPI",
     client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "api/"));
 
+var urlCatalogApi = string.Empty;
+
+if (builder.HostEnvironment.IsDevelopment())
+{
+    urlCatalogApi = "https://localhost:7285/api/";
+}
+if (builder.HostEnvironment.IsProduction())
+{
+    urlCatalogApi = "https://imaginacatalogapi.azurewebsites.net/api/";
+}
+
 builder.Services.AddHttpClient("CatalogMusic.API",
-        client => client.BaseAddress = new Uri(@"https://localhost:7285/api/"));
+        client => client.BaseAddress = new Uri(urlCatalogApi));
 
 builder.Services.AddApiAuthorization();
 builder.Services.AddBlazoredToast();
